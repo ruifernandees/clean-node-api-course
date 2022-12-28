9ee11f1f0d66349f529956b40326cfda5186c07d
ruifernandees <dev.ruifernandes@gmail.com>
2022-12-28 10:31:20 -0300

    feat: ensure mongodb reconnect if client is down

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,9 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrlTest: process.env.MONGO_URL_TEST,
   port: process.env.PORT || 5050
 }
